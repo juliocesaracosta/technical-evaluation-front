25b05275833a81e74f4a9ecdd5372b2f4aeb214f
julio cesar <jcesartrabajo@gmail.com>
2024-04-23 20:04:13 -0700

    se ajusto agrego el nombre del usuario al navbar

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -26,33 +26,21 @@ function Nav() {
         <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Content</span>
       </Navbar.Brand>
       <div className="flex md:order-2">
-        {user ? '' :
+        { user ?
+          <Navbar.Collapse>
+            <Navbar.Link href="" className="text-red-600"><Link to="">Bienvenido {user.name}</Link></Navbar.Link>
+            <Navbar.Link href=""><Link to="">Cerrar Sesion</Link></Navbar.Link>
+          </Navbar.Collapse>
+        :
+
         <Navbar.Collapse>
           <Navbar.Link href=""><Link to="/login">Ingresar</Link></Navbar.Link>
-
         </Navbar.Collapse>}
-        <Dropdown
-          arrowIcon={false}
-          inline
-          label={
-            <Avatar alt="User settings" img="https://flowbite.com/docs/images/people/profile-picture-5.jpg" rounded />
-          }
-        >
-          <Dropdown.Header>
-            <span className="block text-sm">Bonnie Green</span>
-            <span className="block truncate text-sm font-medium">name@flowbite.com</span>
-          </Dropdown.Header>
-          <Dropdown.Item>Dashboard</Dropdown.Item>
-          <Dropdown.Item>Settings</Dropdown.Item>
-          <Dropdown.Item>Earnings</Dropdown.Item>
-          <Dropdown.Divider />
-          <Dropdown.Item>Sign out</Dropdown.Item>
-          </Dropdown>
-        <Navbar.Toggle />
       </div>
+
       <Navbar.Collapse>
-        <Navbar.Link href=""><Link to="/">Home</Link></Navbar.Link>
-        <Navbar.Link href="#"><Link to="/profile">Profile</Link></Navbar.Link>
+        <Navbar.Link href=""><Link to="/">Inicio</Link></Navbar.Link>
+        <Navbar.Link href="#"><Link to="/profile">Mi cuenta</Link></Navbar.Link>
         {haveRol ? <Navbar.Link href="#"><Link to="/administration">Administration</Link></Navbar.Link> : ''}
       </Navbar.Collapse>
     </Navbar>
