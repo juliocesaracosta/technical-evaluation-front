9b6d48509c0163229a30ea324095a8daadd31cdf
Julio Cesar Acosta <julio.acosta@coppel.com>
2024-04-23 22:33:22 -0700

    se agrega validacion para controlar el acceso por la url del navegador

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate} from 'react-router-dom';
 import { Avatar, Dropdown, Navbar } from "flowbite-react";
 
 
 function Nav() {
   const [haveRol, setRol] = useState(null);
   const [user, setUser] = useState(null);
+  const navigate = useNavigate();
 
   useEffect(() => {
     const tpmuser = sessionStorage.getItem('user');
@@ -19,6 +20,15 @@ function Nav() {
          true: false)
     }
   }, [user]);
+
+  const logOut = () => {
+    sessionStorage.removeItem('token');
+    sessionStorage.removeItem('user');
+    navigate("/login");
+    console.log('Logout')
+  } 
+
+
   return (
     <Navbar fluid className='nav-important'>
       <Navbar.Brand href="https://flowbite-react.com">
@@ -29,7 +39,7 @@ function Nav() {
         { user ?
           <Navbar.Collapse>
             <Navbar.Link href="" className="text-red-600"><Link to="">Bienvenido {user.name}</Link></Navbar.Link>
-            <Navbar.Link href=""><Link to="">Cerrar Sesion</Link></Navbar.Link>
+            <Navbar.Link href=""><Link to="" onClick={logOut}>Cerrar Sesion</Link></Navbar.Link>
           </Navbar.Collapse>
         :
 
